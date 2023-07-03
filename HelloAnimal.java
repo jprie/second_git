ec22a5d2b67bb722ffb6aca2955280f89efddb1d
Johannes Priebsch <johannes@priebsch.at>
2023-07-03 12:10:45 +0200

    new feature: hello animal

---
+++ HelloAnimal.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+public static void main(String[] args) {
+	
+				System.out.println("Hello Animal");
+
+				var animals = { new Cat("Mia"), new Dog("Ami") };
+
+				for (var animal : animals) {
+					System.out.println(animal.getName());
+				
+				}
+
+}
