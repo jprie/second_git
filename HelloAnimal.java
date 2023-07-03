1be38e29e5282bbc5f250ac857c08b1f877fe16c
Johannes Priebsch <johannes@priebsch.at>
2023-07-03 12:27:51 +0200

    change cat name

--- HelloAnimal.java
+++ HelloAnimal.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
 	
 				System.out.println("Hello Animal");
 
-				var animals = { new Cat("Mia"), new Dog("Ami") };
+				var animals = { new Cat("Mutzi"), new Dog("Ami") };
 
 				for (var animal : animals) {
 					System.out.println(animal.getName());
