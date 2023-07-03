ab5e1ce25f02fcef8388702a812abd4b31979377
Johannes Priebsch <johannes@priebsch.at>
2023-07-03 13:39:02 +0200

    change output to funny

--- HelloAnimal.java
+++ HelloAnimal.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 public static void main(String[] args) {
 	
-				System.out.println("Hello Animal");
+				System.out.println("Hello funny Animal");
 
 				var animals = { new Cat("Mutzi"), new Dog("Ami") };
 
